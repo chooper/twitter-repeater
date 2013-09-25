febc99be6e8886e9f714f9bdce2073af2ec4062a
Charles Hooper <chooper@plumata.com>
2013-09-25 01:50:49 -0700

    Improve logging, fixes #6

--- repeater.py
+++ repeater.py
@@ -17,6 +17,10 @@ import tweepy
 # import exceptions
 from urllib2 import HTTPError
 
+def log(**kwargs):
+    print ' '.join( "{0}={1}".format(k,v) for k,v in sorted(kwargs.items()) )
+
+
 def debug_print(text):
     """Print text if debugging mode is on"""
     if os.environ.get('DEBUG'):
@@ -26,23 +30,23 @@ def debug_print(text):
 def careful_retweet(api,reply):
     """Perform retweets while avoiding loops and spam"""
 
-    username = os.environ.get('TW_USERNAME')
+    log(at='careful_rt_start')
 
-    debug_print('Preparing to retweet #%d' % (reply.id,))
+    username = os.environ.get('TW_USERNAME')
     normalized_tweet = reply.text.lower().strip()
 
     # Don't try to retweet our own tweets
     if reply.user.screen_name.lower() == username.lower():
-        debug_print("Not retweeting: Own tweet")
+        log(at='filter', reason='is_my_tweet', tweet=reply.id)
         return
 
     # HACK: Don't retweet if tweet contains more usernames than words (roughly)
     username_count = normalized_tweet.count('@')
     if username_count >= len(normalized_tweet.split()) - username_count:
-        debug_print("Not retweeting: Failed @ test")
+        log(at='filter', reason='too_many_usernames', tweet=reply.id)
         return
 
-    debug_print('Retweeting #%d' % (reply.id,))
+    log(at='retweet', tweet=reply.id)
     return api.retweet(id=reply.id)
 
 
@@ -55,13 +59,20 @@ def validate_env():
         'TW_ACCESS_TOKEN_SECRET',
         ]
 
+    # Check for missing env vars
     for key in keys:
         v = os.environ.get(key)
         if not v:
+            log(at='validate_env', status='missing', var=key)
             raise ValueError("Missing ENV var: {0}".format(key))
+
+    # Log success
+    log(at='validate_env', status='ok')
 
 
 def main():
+    log(at='main')
+
     validate_env()
 
     consumer_key      = os.environ.get('TW_CONSUMER_KEY')
@@ -75,43 +86,44 @@ def main():
 
     api = tweepy.API(auth_handler=auth, secure=True, retry_count=3)
 
-    debug_print('Loading friends list')
+    log(at='load_friends_start')
     friends = api.friends_ids()
-    debug_print('Friend list loaded, size: %d' % len(friends))
+    log(at='load_friends_finish', friends=len(friends))
 
-    try:
-        debug_print('Retrieving mentions')
-        replies = api.mentions_timeline()
-    except Exception, e:    # quit on error here
-        print e
-        exit(1)
+    log(at='load_mentions_start')
+    replies = api.mentions_timeline()
+    log(at='load_mentions_finish', mentions=len(replies))
 
     # want these in ascending order, api orders them descending
     replies.reverse()
 
     for reply in replies:
-        # ignore twet if we've already tweeted it
+        # ignore tweet if we've already tweeted it
         if reply.retweeted:
+            log(at='ignore', tweet=reply.id, reason='already_retweeted')
             continue
 
         # ignore tweet if it's not from someone we follow
         if reply.user.id not in friends:
+            log(at='ignore', tweet=reply.id, reason='not_followed')
             continue
 
         try:
             careful_retweet(api,reply)
         except HTTPError, e:
-            print e.code()
-            print e.read()
+            log(at='rt_error', klass='HTTPError', code=e.code(), body_size=len(e.read()))
+            debug_log(e.code())
+            debug_log(e.read())
         except Exception, e:
-            print 'e: %s' % e
-            print repr(e)
+            log(at='rt_error', klass='Exception', msg="'{0}'".format(str(e)))
+            debug_log('e: %s' % e)
 
-    debug_print('Exiting cleanly')
+    log(at='finish', status='ok')
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
+        log(at='keyboard_interrupt')
         quit()
 
